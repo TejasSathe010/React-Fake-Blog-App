f088fff655d3f9fcb69c8564c143748e44f44283
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2022-02-08 21:13:59 +0530

    Add diff components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import NewPost from './components/NewPost';
+import PostPage from './components/PostPage';
+import About from './components/About';
+import Missing from './components/Missing';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/post" element={<NewPost />} />
+          <Route path="/post/:id" element={<PostPage />}/>
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/NewPost.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewPost = () => {
+  return <div>
+      <h1>NewPost</h1>
+  </div>;
+};
+
+export default NewPost;
